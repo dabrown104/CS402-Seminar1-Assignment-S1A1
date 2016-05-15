5df598a4e8882c9926ca202838e29781f8c7be80
dabrown104 <raven122685@yahoo.com>
2016-05-14 22:40:35 -0400

    CS402-Assignment-S1A1
    These are the general templates for seminar 1 assignments.

---
+++ BlankTemplate10App/BlankTemplate10App/App.xaml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml.Data;
+
+namespace BlankTemplate10App
+{
+    /// Documentation on APIs used in this page:
+    /// https://github.com/Windows-XAML/Template10/wiki
+
+    [Bindable]
+    sealed partial class App : Template10.Common.BootStrapper
+    {
+        public App() {
+            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
+Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
+Microsoft.ApplicationInsights.WindowsCollectors.Session);
+            InitializeComponent(); }
+
+        public override async Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            await Task.CompletedTask;
+        }
+
+        public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            NavigationService.Navigate(typeof(Views.MainPage));
+            await Task.CompletedTask;
+        }
+    }
+}
+
---
+++ Minimal10App/Minimal10App/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Minimal10App.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Minimal10App.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
